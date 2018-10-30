a2844c151766d9ddd3a608e3ef2756c3d0c6f85e
Maggi Sledjeski <sledjeski_ma@gsc_405_05.mercer.edu>
2018-10-30 12:23:56 -0400

    adding initial keyboard and mouse

---
+++ keyboardmouse.cpp
@@ -0,0 +1,35 @@
+#include "includes.h"
+#include "prototypes.h"
+
+
+void mouse(int button, int state, int x, int y)
+{
+	extern float deltaSpin;
+ 
+	switch (button) {
+        case GLUT_LEFT_BUTTON:
+            if (state == GLUT_DOWN)
+            {
+                deltaSpin = deltaSpin - 1.0;
+                glutIdleFunc(spinDisplay);
+            }
+            break;
+        case GLUT_RIGHT_BUTTON:
+            if (state == GLUT_DOWN)
+            {
+                deltaSpin = deltaSpin + 1.0;
+                glutIdleFunc(spinDisplay);
+            }
+            break;
+        default:
+            break;	
+	}
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+	//exits the program if q or Q is clicked
+	if(key == 'q' || key == 'Q') {
+		exit(0);
+    }
+}
